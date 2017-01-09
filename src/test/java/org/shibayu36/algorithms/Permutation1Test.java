598c10949fb16a80fd5826a88a0625882face1db
shibayu36 <shibayu36@gmail.com>
2017-01-09 11:22:24 +0900

    データ構造系のものもこちらに移す

---
+++ src/test/java/org/shibayu36/algorithms/Permutation1Test.java
@@ -0,0 +1,15 @@
+package org.shibayu36.algorithms;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by shibayu36 on 2016/12/19.
+ */
+public class Permutation1Test {
+    @Test
+    public void make() throws Exception {
+        Permutation1.make(Arrays.asList(1, 2, 3, 4));
+    }
+
+}
---
+++ src/test/java/org/shibayu36/algorithms/Permutation2Test.java
@@ -0,0 +1,19 @@
+package org.shibayu36.algorithms;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shibayu36 on 2016/12/20.
+ */
+public class Permutation2Test {
+    @Test
+    public void make() throws Exception {
+        List<List<Integer>> result = Permutation2.make(Arrays.asList(1, 2, 3));
+        System.out.println(result.toString());
+    }
+
+}
