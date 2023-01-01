80e7e2799bb7c57d70b1329c127153f34773b54e
shibayu36 <shibayu36@gmail.com>
2023-01-01 15:20:55 +0900

    Implement WeightedGraph data structure

---
+++ ruby/lib/weighted_graph.rb
@@ -0,0 +1,32 @@
+class WeightedGraph
+  def initialize
+    @graph = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def add_edge(from, to, weight)
+    @graph[from].push(Edge.new(from, to, weight))
+    @graph[to].push(Edge.new(to, from, weight))
+  end
+
+  def add_directed_edge(from, to, weight)
+    @graph[from].push(Edge.new(from, to, weight))
+  end
+
+  def to_h
+    @graph.values.flat_map do |edges|
+      edges.map do |edge|
+        { from: edge.from, to: edge.to, weight: edge.weight }
+      end
+    end
+  end
+
+  class Edge
+    attr_reader :from, :to, :weight
+
+    def initialize(from, to, weight)
+      @from = from
+      @to = to
+      @weight = weight
+    end
+  end
+end
---
+++ ruby/spec/weighted_graph_spec.rb
@@ -0,0 +1,49 @@
+require 'weighted_graph'
+
+describe WeightedGraph do
+  it 'constructs undirected graph' do
+    graph = WeightedGraph.new
+    graph.add_edge(1, 2, 2)
+    graph.add_edge(1, 3, 1)
+    graph.add_edge(2, 4, 5)
+    graph.add_edge(3, 5, 4)
+    graph.add_edge(4, 5, 1)
+    graph.add_edge(4, 6, 2)
+    expect(graph.to_h).to eq(
+      [
+        { from: 1, to: 2, weight: 2 },
+        { from: 1, to: 3, weight: 1 },
+        { from: 2, to: 1, weight: 2 },
+        { from: 2, to: 4, weight: 5 },
+        { from: 3, to: 1, weight: 1 },
+        { from: 3, to: 5, weight: 4 },
+        { from: 4, to: 2, weight: 5 },
+        { from: 4, to: 5, weight: 1 },
+        { from: 4, to: 6, weight: 2 },
+        { from: 5, to: 3, weight: 4 },
+        { from: 5, to: 4, weight: 1 },
+        { from: 6, to: 4, weight: 2 }
+      ]
+    )
+  end
+
+  it 'constructs directed graph' do
+    graph = WeightedGraph.new
+    graph.add_directed_edge(1, 2, 2)
+    graph.add_directed_edge(1, 3, 1)
+    graph.add_directed_edge(2, 4, 5)
+    graph.add_directed_edge(3, 5, 4)
+    graph.add_directed_edge(4, 5, 1)
+    graph.add_directed_edge(4, 6, 2)
+    expect(graph.to_h).to eq(
+      [
+        { from: 1, to: 2, weight: 2 },
+        { from: 1, to: 3, weight: 1 },
+        { from: 2, to: 4, weight: 5 },
+        { from: 3, to: 5, weight: 4 },
+        { from: 4, to: 5, weight: 1 },
+        { from: 4, to: 6, weight: 2 }
+      ]
+    )
+  end
+end
