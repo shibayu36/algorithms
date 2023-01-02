3ee9acd68f3f7f422d97118d49fb2cfc2f650052
shibayu36 <shibayu36@gmail.com>
2023-01-02 18:55:06 +0900

    early break if it was not changed

--- ruby/lib/bellman_ford.rb
+++ ruby/lib/bellman_ford.rb
@@ -8,14 +8,18 @@ class BellmanFord
     @cost[from] = 0
 
     (0..@graph.points.length - 1).each do |_i|
+      is_changed = false
+
       @graph.edges.each do |edge|
         candidate_cost = @cost[edge[:from]] + edge[:weight]
         if candidate_cost < @cost[edge[:to]]
           @cost[edge[:to]] = candidate_cost
+          is_changed = true
           # TODO: negative loop detection
         end
       end
-      # TODO: no longer need to iterate if no change
+
+      break unless is_changed
     end
 
     @cost[to]
