6c3f10d05ea5d80de4f300e61646a370997c43ef
shibayu36 <shibayu36@gmail.com>
2023-02-14 15:36:54 +0900

    Make it possible to customize the heap cmp

---
+++ ruby/lib/customize_heap.rb
@@ -0,0 +1,72 @@
+class Heap
+  attr_reader :nodes
+
+  def initialize(arr)
+    @nodes = []
+    arr.each { |val| push(val) }
+  end
+
+  def size
+    @nodes.size
+  end
+
+  def push(val)
+    @nodes.push(val)
+
+    current_index = size - 1
+    parent_index = parent_of(size - 1)
+
+    while current_index > 0 && !priority_cmp(parent_index, current_index)
+      swap(parent_index, current_index)
+      current_index = parent_index
+      parent_index = parent_of(current_index)
+    end
+  end
+
+  def pop
+    return @nodes.pop if size <= 1
+
+    res = @nodes[0]
+
+    @nodes[0] = @nodes.pop
+
+    current_index = 0
+    child_index = priority_child_of(current_index)
+
+    while child_index && priority_cmp(child_index, current_index)
+      swap(child_index, current_index)
+      current_index = child_index
+      child_index = priority_child_of(current_index)
+    end
+
+    res
+  end
+
+  private
+
+  def parent_of(index)
+    (index - 1) / 2
+  end
+
+  def priority_child_of(index)
+    left_child_index = (2 * index) + 1
+    right_child_index = (2 * index) + 2
+
+    left_val = nodes[left_child_index]
+    right_val = nodes[right_child_index]
+
+    if left_val && right_val
+      priority_cmp(left_child_index, right_child_index) ? left_child_index : right_child_index
+    elsif left_val
+      left_child_index
+    end
+  end
+
+  def priority_cmp(i, j)
+    @nodes[i] >= @nodes[j]
+  end
+
+  def swap(i, j)
+    @nodes[i], @nodes[j] = @nodes[j], @nodes[i]
+  end
+end
