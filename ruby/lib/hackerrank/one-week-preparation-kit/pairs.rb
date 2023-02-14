7768a2a0bf3fbaaea29cf3e34b539a13a49c50f2
shibayu36 <shibayu36@gmail.com>
2023-02-14 14:56:35 +0900

    Solve pairs using set

---
+++ ruby/lib/hackerrank/one-week-preparation-kit/pairs.rb
@@ -0,0 +1,45 @@
+require 'json'
+require 'stringio'
+require 'set'
+
+#
+# Complete the 'pairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def pairs(k, arr)
+  set = Set.new(arr)
+
+  result = 0
+  arr.each do |num|
+    target = num + k
+    if set.include?(target)
+      result += 1
+    end
+  end
+
+  result
+end
+
+# puts(pairs(2, [1, 5, 3, 4, 2]))
+
+fptr = File.open(ENV.fetch('OUTPUT_PATH', nil), 'w')
+
+first_multiple_input = gets.rstrip.split
+
+n = first_multiple_input[0].to_i
+
+k = first_multiple_input[1].to_i
+
+arr = gets.rstrip.split.map(&:to_i)
+
+result = pairs k, arr
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close
