6caded5f3d6e827fe59f39f92d8b0c3687065492
shibayu36 <shibayu36@gmail.com>
2023-03-03 15:18:57 +0900

    without using new LinkedList

--- ruby/lib/hackerrank/one-week-again/merge-two-sorted-list.rb
+++ ruby/lib/hackerrank/one-week-again/merge-two-sorted-list.rb
@@ -1,9 +1,17 @@
 class LinkedList
   attr_reader :head, :tail
 
-  def initialize
+  def initialize(node = nil)
     @head = nil
     @tail = nil
+
+    if node
+      @head = node
+      while node
+        @tail = node
+        node = node.next
+      end
+    end
   end
 
   def push(value)
@@ -44,32 +52,67 @@ def merge_lists(l1, l2)
   return l1 unless l2.head
   return l2 unless l1.head
 
-  result = LinkedList.new
-
+  root = LinkedListNode.new(nil)
+  current = root
   p1 = l1.head
   p2 = l2.head
 
   while p1 && p2
     if p1.val <= p2.val
-      result.push(p1.val)
+      current.next = p1
       p1 = p1.next
+      current = current.next
     else
-      result.push(p2.val)
+      current.next = p2
       p2 = p2.next
+      current = current.next
     end
   end
 
   while p1
-    result.push(p1.val)
+    current.next = p1
     p1 = p1.next
+    current = current.next
   end
   while p2
-    result.push(p2.val)
+    current.next = p2
     p2 = p2.next
+    current = current.next
   end
 
-  result
+  LinkedList.new(root.next)
 end
+
+# def merge_lists(l1, l2)
+#   return l1 unless l2.head
+#   return l2 unless l1.head
+
+#   result = LinkedList.new
+
+#   p1 = l1.head
+#   p2 = l2.head
+
+#   while p1 && p2
+#     if p1.val <= p2.val
+#       result.push(p1.val)
+#       p1 = p1.next
+#     else
+#       result.push(p2.val)
+#       p2 = p2.next
+#     end
+#   end
+
+#   while p1
+#     result.push(p1.val)
+#     p1 = p1.next
+#   end
+#   while p2
+#     result.push(p2.val)
+#     p2 = p2.next
+#   end
+
+#   result
+# end
 
 test_case_num = gets.strip.to_i
 
